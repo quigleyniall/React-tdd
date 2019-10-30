157efea1059e560689eaebe543ceb6c29af9304c
Niall Quigley <niallquigley92@gmail.com>
2019-10-30 13:52:20 +0000

    Refactor code

--- src/NewRestaurantForm.js
+++ src/NewRestaurantForm.js
@@ -20,19 +20,14 @@ class NewRestaurantForm extends React.Component {
     return (
       <Row>
         <Input
-          s={12}
-          m={8}
-          l={8}
+          s={12} m={8} l={8}
           label="Restaurant Name"
           value={inputText}
           onChange={this.handleTextChange}
           data-test="newRestaurantName" />
         <Button
-          s={12}
-          m={4}
-          l={82}
+          s={12} m={4} l={82}
           data-test="saveNewRestaurantButton"
-          modal="close"
           onClick={this.handleSave}>Save</Button>
       </Row>
     );
--- src/RestaurantList.js
+++ src/RestaurantList.js
@@ -3,21 +3,28 @@ import { Collection, CollectionItem } from 'react-materialize';
 
 const RestaurantList = ({ restaurantNames }) => (
   <Collection header="Restaurants">
-    {
-      restaurantNames.length === 0
-        ?
-        (
-          <CollectionItem style={{ color: 'grey' }}>No Restaurants Added yet</CollectionItem>
-        )
-        : (
-          restaurantNames.map(restaurantName => (
-            <CollectionItem key={restaurantName}>
-              {restaurantName}
-            </CollectionItem>
-          ))
-        )
-    }
+    <RestaurantItems restaurantNames={restaurantNames} />
   </Collection>
+);
+
+const RestaurantItems = ({ restaurantNames }) => (
+  restaurantNames.length === 0
+    ? <NoRestaurantItems />
+    : <SomeRestaurantItems restaurantNames={restaurantNames} />
+);
+
+const NoRestaurantItems = () => (
+  <CollectionItem style={{ color: 'grey' }}>
+    No Restaurants Added yet
+  </CollectionItem>
+);
+
+const SomeRestaurantItems = ({ restaurantNames }) => (
+  restaurantNames.map(restaurantName => (
+    <CollectionItem key={restaurantName}>
+      {restaurantName}
+    </CollectionItem>
+  ))
 );
 
 export default RestaurantList;
