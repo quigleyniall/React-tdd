3d2badba7d74c9801fedd6f0345f63775d8a2a02
Niall Quigley <niallquigley92@gmail.com>
2019-10-31 19:46:08 +0000

    Reformat file

--- cypress/integration/adding_a_restaurant.spec.js
+++ cypress/integration/adding_a_restaurant.spec.js
@@ -11,71 +11,37 @@ describe('adding a restaurant', () => {
   });
 
   function modalNotShownAtTheStart() {
-    cy.get('[data-testid="newRestaurantName"]')
-      .should('not.be.visible');
+    cy.get('[data-testid="newRestaurantName"]').should('not.be.visible');
   }
 
   function modalCanBeCanceled() {
-    cy.get('[data-testid="addRestaurantButton"]')
-      .click();
-
-    cy.get('[data-testid="cancelModalButton"]')
-      .click();
-
-    cy.get('[data-testid="newRestaurantName"]')
-      .should('not.be.visible');
+    cy.get('[data-testid="addRestaurantButton"]').click();
+    cy.get('[data-testid="cancelModalButton"]').click();
+    cy.get('[data-testid="newRestaurantName"]').should('not.be.visible');
   }
 
   function modalDisplaysValidationErrors() {
-    cy.get('[data-testid="addRestaurantButton"]')
-      .click();
-
-    cy.get('[data-testid="saveNewRestaurantButton"]')
-      .click();
-
-    cy.get('label[for="restaurantName"][data-error="Cannot be blank"]')
-      .should('be.visible');
-
-    cy.get('[data-testid="cancelModalButton"]')
-      .click();
+    cy.get('[data-testid="addRestaurantButton"]').click();
+    cy.get('[data-testid="saveNewRestaurantButton"]').click();
+    cy.get('label[for="restaurantName"][data-error="Cannot be blank"]').should('be.visible');
+    cy.get('[data-testid="cancelModalButton"]').click();
   }
 
   function modalClearsOutValidationErrorWhenClosed() {
-    cy.get('[data-testid="addRestaurantButton"]')
-      .click();
-
-    cy.get('[data-testid="saveNewRestaurantButton"]')
-      .click();
-
-    cy.get('label[for="restaurantName"][data-error="Cannot be blank"]')
-      .should('be.visible');
-
-    cy.get('[data-testid="cancelModalButton"]')
-      .click();
-
-    cy.get('[data-testid="addRestaurantButton"]')
-      .click();
-
-    cy.get('label[for="restaurantName"][data-error="Cannot be blank"]')
-      .should('not.be.visible');
-
-    cy.get('[data-testid="cancelModalButton"]')
-      .click();
+    cy.get('[data-testid="addRestaurantButton"]').click();
+    cy.get('[data-testid="saveNewRestaurantButton"]').click();
+    cy.get('label[for="restaurantName"][data-error="Cannot be blank"]').should('be.visible');
+    cy.get('[data-testid="cancelModalButton"]').click();
+    cy.get('[data-testid="addRestaurantButton"]').click();
+    cy.get('label[for="restaurantName"][data-error="Cannot be blank"]').should('not.be.visible');
+    cy.get('[data-testid="cancelModalButton"]').click();
   }
 
   function modalAllowsTypingRestaurant(restaurantName) {
-    cy.get('[data-testid="addRestaurantButton"]')
-      .click();
-
-    cy.get('[data-testid="newRestaurantName"]')
-      .type(restaurantName);
-
-    cy.get('[data-testid="saveNewRestaurantButton"]')
-      .click();
-
-    cy.get('[data-testid="newRestaurantName"]')
-      .should('not.be.visible');
-
+    cy.get('[data-testid="addRestaurantButton"]').click();
+    cy.get('[data-testid="newRestaurantName"]').type(restaurantName);
+    cy.get('[data-testid="saveNewRestaurantButton"]').click();
+    cy.get('[data-testid="newRestaurantName"]').should('not.be.visible');
     cy.contains(restaurantName);
   }
 });
